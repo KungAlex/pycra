f93ec05be40a029b3e9e9183fbc6822fcd7b7df0
kungalex <alexander.kleinschmidt@smail.fh-koeln.de>
2017-11-01 05:54:33 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+### pyrcra
+
+Python Challenge Response Authentication with PBKDF2
+
+Example:
+
+   TODO
+
+
+copyright: (c) Copyright 2017 by Kungalex.
+License: MIT
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+      name='pycra',
+      packages=['pycra'],
+      version='0.0.1',
+      description='Python Challenge Response Authentication with PBKDF2',
+
+      author='Alexander Kleinschmidt (kungalex)',
+      author_email='kung4lex@gmail.ooo',
+      license='MIT',
+
+      url='http://github.com/kungalex/pycra',
+      download_url='https://github.com/kungalex/pycra/archive/0.0.1.tar.gz',
+
+      keywords=['challenge-response', 'Authentication', 'PBKDF2'],
+      classifiers=[],
+      )
+
